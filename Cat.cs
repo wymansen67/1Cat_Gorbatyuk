d2385965f99d51c0f534d85e888b50b0fdca8a29
wymansen67 <DenisGorbatuk77@gmail.com>
2023-05-25 14:19:57 +0500

    Добавьте файлы проекта.

---
+++ Cat.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Cat
+    {
+        public Cat(string CatName)
+        {
+            Name = CatName;
+        }
+
+        private string name;
+        public string nameP;
+        private double weight;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                bool OnlyLetters = true;
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+
+        public string Weight
+        {
+            get { return weight.ToString(); }
+            set
+            {
+                bool valid = true;
+                if (value.Contains(".")) valid = false;
+                else if (double.Parse(value) <= 0) valid = false;                
+                if (valid)
+                {
+                    foreach (var ch in value)
+                    {
+                        if (!char.IsDigit(ch) && ch != ',') { valid = false; break; }
+                    }
+                }
+                if (valid)
+                {
+                    weight = double.Parse(value);
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - недопустимое значение веса!");
+                }
+            }
+        }
+
+        public void Meow()
+        {
+            Console.Write($"{name}: МЯУ!");
+        }
+
+        public void Fat()
+        {
+            Console.WriteLine($" Вес: {weight}");
+        }
+
+        /*public void SetCatName(string CatName)
+        {
+
+            bool OnlyLetters = true;
+
+            foreach (var ch in CatName)
+            {
+                if (!char.IsLetter(ch)) { OnlyLetters = false; break; }
+            }
+
+            if (OnlyLetters) name = CatName;
+            else Console.WriteLine($"{CatName} - неправильное имя!!!");
+        }*/
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                        
+            Cat murzik = new Cat("Мурзик");
+            Cat barsik = new Cat("Барсук");
+            Cat barsik123 = new Cat("Барсик123");
+            murzik.Meow();
+            murzik.Weight = "12,5";
+            murzik.Fat();
+            barsik.Meow();
+            barsik.Fat();
+            barsik.Name = "Барсик";
+            barsik.Weight = "12,5";
+            barsik.Meow();
+            barsik.Fat();
+            murzik.Name = "12421";
+            murzik.Weight = "-9,7";
+            murzik.Meow();
+            murzik.Fat();
+            /*Console.WriteLine("Введите вес барсика");
+            barsik.Weight = Console.ReadLine();
+            Console.WriteLine(barsik.Weight);*/
+            Console.ReadKey();
+        }
+    }
+}
